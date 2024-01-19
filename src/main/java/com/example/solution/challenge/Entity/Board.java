4da6d08c7c96a88930af6bf9e9164a7e26e8accf
YU JEONG SEON <yjs000616@gmail.com>
2024-01-19 20:48:26 +0900

    [MOD] Board 중복 import 제거

--- src/main/java/com/example/solution/challenge/Entity/Board.java
+++ src/main/java/com/example/solution/challenge/Entity/Board.java
@@ -7,12 +7,6 @@ import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
-
 @Entity
 @Getter @Setter
 
