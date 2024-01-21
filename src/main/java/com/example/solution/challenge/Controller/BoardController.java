02ae329070cca7f48d8e492246d5dada113a6964
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-01-21 12:28:55 +0900

    [joon/MOD] BoardController - 신규 게시글 생성 및 전체 게시글 조회

---
+++ src/main/java/com/example/solution/challenge/Controller/BoardController.java
@@ -0,0 +1,48 @@
+// Controller  BoardController
+
+
+
+package com.example.solution.challenge.Controller;
+
+import com.example.solution.challenge.Dto.BoardDto;
+import com.example.solution.challenge.Entity.Board;
+import com.example.solution.challenge.Service.BoardService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/boards")
+public class BoardController {
+
+    private final BoardService boardService;
+
+    @Autowired
+    public BoardController(BoardService boardService) {
+        this.boardService = boardService;
+    }
+
+    // 모든 게시글 조회
+    @GetMapping
+    public List<Board> getAllBoards() {
+        return boardService.getAllBoards();
+    }
+
+    // 새로운 게시글 생성
+    @PostMapping
+    public ResponseEntity<Board> createBoard(@RequestBody BoardDto boardDto) {
+        Board newBoard = boardService.createBoard(boardDto);
+        return ResponseEntity.ok(newBoard);
+    }
+
+    // 특정 ID의 게시글 조회
+
+    // 특정 ID의 게시글 업데이트
+
+    // 특정 ID의 게시글 삭제
+
+}
