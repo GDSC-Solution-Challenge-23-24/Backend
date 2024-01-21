bd1ee5bbab1b89c29b7b73808f7b450dace66e62
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-01-21 12:31:07 +0900

    [joon/MOD] BoardDto

---
+++ src/main/java/com/example/solution/challenge/Dto/BoardDto.java
@@ -0,0 +1,16 @@
+// Dto  BoardDto
+
+
+package com.example.solution.challenge.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+
+public class BoardDto {
+
+    private String title;
+    private String content;
+
+}
