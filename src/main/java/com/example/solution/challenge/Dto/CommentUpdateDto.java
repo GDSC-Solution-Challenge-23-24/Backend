9d797597d58f2d2718ab153aac2fbf3931aaf5e8
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-01-21 12:22:59 +0900

    [joon/DEL] Comment, Board 관련 파일 삭제 (오늘 내로 수정본 업로드하겠습니다)

--- src/main/java/com/example/solution/challenge/Dto/CommentUpdateDto.java
+++
@@ -1,9 +0,0 @@
-package com.example.solution.challenge.Dto;
-
-import lombok.Getter;
-import lombok.Setter;
-
-@Getter @Setter
-public class CommentUpdateDto {
-    private String content;
-}
--- src/main/java/com/example/solution/challenge/Repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.solution.challenge.Repository;
-
-import com.example.solution.challenge.Entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Long> {
-
-}
