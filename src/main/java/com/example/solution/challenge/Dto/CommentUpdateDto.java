ffd2acc5941dd00da39e2cbf07bf5e7fef2c7d89
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-01-27 01:59:43 +0900

    [joon/MOD] CommentUpdate Dto

---
+++ src/main/java/com/example/solution/challenge/Dto/CommentUpdateDto.java
@@ -0,0 +1,11 @@
+// Dto  CommentUpdateDto
+
+package com.example.solution.challenge.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class CommentUpdateDto {
+    private String content;
+}
