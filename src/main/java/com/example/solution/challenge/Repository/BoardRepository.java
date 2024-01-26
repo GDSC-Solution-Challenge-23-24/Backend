fe822ee4078b7ae43027ed59089785879ad19fa5
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-01-27 01:49:17 +0900

    [joon/MOD] Board Repository

---
+++ src/main/java/com/example/solution/challenge/Repository/BoardRepository.java
@@ -0,0 +1,11 @@
+// Repository  BoardRepository
+
+
+package com.example.solution.challenge.Repository;
+
+import com.example.solution.challenge.Entity.Board;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BoardRepository extends JpaRepository<Board, Long> {
+
+}
