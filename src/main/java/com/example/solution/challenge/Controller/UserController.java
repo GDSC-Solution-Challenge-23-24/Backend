70df3d235c52ba30a2bcd2590690142456836e5f
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-02-01 23:00:10 +0900

    [joon/MOD] User Controller

---
+++ src/main/java/com/example/solution/challenge/Controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.solution.challenge.Controller;
+
+import com.example.study.Dto.UserDto;
+import com.example.study.Service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    public ResponseEntity<String> registerUser(@RequestBody UserDto userDto) {
+        userService.registerUser(userDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body("회원가입 성공");
+    }
+
+    public ResponseEntity<String> loginUser(@RequestBody UserDto userDto) {
+        return userService.loginUser(userDto)
+                .map(user -> ResponseEntity.ok("로그인 성공. 안녕하세요, " + userDto.getUsername()))
+                .orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("아이디나 비밀번호가 잘못되었습니다."));
+    }
+}
