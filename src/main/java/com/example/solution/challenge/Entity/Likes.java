50ac1f0fca78da4b4beab968a87c7e820e0b08a2
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-02-01 23:26:17 +0900

    [joon/MOD] Likes Entity

---
+++ src/main/java/com/example/solution/challenge/Entity/Likes.java
@@ -0,0 +1,32 @@
+package com.example.solution.challenge.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+@Getter
+@Setter
+public class Likes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long likesId;
+
+    @ManyToOne(targetEntity = Board.class)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne(targetEntity = User.class)
+    @JoinColumn(name = "user_id")
+    private User user;
+}
