877201500b5438b2c0a330e96eec8857d7f22e65
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-02-01 23:05:43 +0900

    [joon/MOD] User DTO

---
+++ src/main/java/com/example/solution/challenge/Dto/UserDto.java
@@ -0,0 +1,11 @@
+package com.example.solution.challenge.Dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+    private String username;
+    private String password;
+}
