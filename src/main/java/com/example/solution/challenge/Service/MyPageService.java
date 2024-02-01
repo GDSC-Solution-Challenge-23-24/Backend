4854ed9ccae40785b2ae2f03696b812b1fc13a7d
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-02-01 23:20:33 +0900

    [joon/MOD] My Page Service

--- src/main/java/com/example/solution/challenge/Service/MyPageService.java
+++ src/main/java/com/example/solution/challenge/Service/MyPageService.java
@@ -1,4 +1,44 @@
 package com.example.solution.challenge.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import com.example.solution.challenge.Entity.Board;
+import com.example.solution.challenge.Repository.UserRepository;
+import com.example.solution.challenge.Repository.LikesRepository;
+import com.example.solution.challenge.Entity.Likes;
+import com.example.solution.challenge.Entity.User;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class MyPageService {
+    private final LikesRepository likeRepository;
+    private final UserRepository userRepository;
+
+    // 좋아요 글 목록 조회
+    public List<Board> getLikedBoards(Long userId) {
+        User user= userRepository.findById(userId).orElseThrow(NullPointerException::new);
+        List<Likes> likedList=likeRepository.findByUser(user);
+        return likedList.stream().map(Likes::getBoard).collect(Collectors.toList());
+    }
+
+    // 닉네임 수정
+    public boolean updateNickname(Long userId, String newNickname) {
+        Optional<User> userOptional = userRepository.findById(userId);
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            user.setUsername(newNickname);
+            userRepository.save(user);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
 }
