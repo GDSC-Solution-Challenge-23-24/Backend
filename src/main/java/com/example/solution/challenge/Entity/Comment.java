8d136ba8324fd6d660f3dc5e6bf7ee9416a0ce0f
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-02-04 16:56:27 +0900

    [joon/MOD] yml

--- src/main/java/com/example/solution/challenge/Entity/Comment.java
+++ src/main/java/com/example/solution/challenge/Entity/Comment.java
@@ -2,19 +2,20 @@
 
 package com.example.solution.challenge.Entity;
 
-//import javax.persistence.Column;
-//import javax.persistence.Entity;
-//import javax.persistence.GeneratedValue;
-//import javax.persistence.GenerationType;
-//import javax.persistence.Id;
-//import javax.persistence.JoinColumn;
-//import javax.persistence.ManyToOne;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
-import jakarta.persistence.*;
-
+ // import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.ColumnDefault;
+
+import javax.persistence.Entity;
 
 @Entity
 @Getter
