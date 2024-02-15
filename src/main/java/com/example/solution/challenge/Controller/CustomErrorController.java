0abb17489c6fadfd64cf3f58c2e7f39d604105cf
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-02-15 21:42:22 +0900

    [joon/MOD] new version swagger error

---
+++ src/main/java/com/example/solution/challenge/Controller/CustomErrorController.java
@@ -0,0 +1,21 @@
+package com.example.solution.challenge.Controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError() {
+        // Provide your custom error page path or logic here
+        return "error";
+    }
+
+    @Override
+    public String getErrorPath() {
+        return "/error";
+    }
+}
+
