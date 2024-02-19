db597a1237e21f5cce239f4afef2d2db9f7d3362
jooon <98799034+SeongJoonhee@users.noreply.github.com>
2024-02-19 14:16:30 +0900

    [joon/MOD] image repository

---
+++ src/main/java/com/example/solution/challenge/Repository/ImageRepository.java
@@ -0,0 +1,10 @@
+package com.example.solution.challenge.Repository;
+
+import com.example.solution.challenge.Entity.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImageRepository extends JpaRepository<Image, Long> {
+
+
+}
+
